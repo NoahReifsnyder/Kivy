9008790edb55eddf0557a640ab20e078d2526aaa
Noah Reifsnyder <ndr217@lehigh.edu>
2018-04-18 18:25:16 -0400

    hi

--- demo/test/main.py
+++ demo/test/main.py
@@ -7,18 +7,21 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 
 class LoginScreen(GridLayout):
-
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
         self.cols = 2
-        self.add_widget(Label(text='User Name'))
+        a=Label(text='User Name')
+        self.add_widget(a)
         self.username = TextInput(multiline=False)
         self.add_widget(self.username)
         self.add_widget(Label(text='password'))
         self.password = TextInput(password=True, multiline=False)
         self.add_widget(self.password)
         print(self.password)
-
+    def on_touch_down(self, touch):
+        if touch.is_double_tap:
+            print(touch,self.password.text)
+            return False
 class MyApp(App):
 
     def build(self):
